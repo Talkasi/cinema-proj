36faf76037db261c64f1e838de402bc85142fe8d
Talkasi <kitalkasi@gmail.com>
2025-05-22 19:08:07 +0300

    Correcting models

--- internal/domain/user.go
+++ internal/domain/user.go
@@ -8,26 +8,3 @@ type User struct {
 	BirthDate    string `json:"birth_date" example:"1990-01-01"`
 	IsAdmin      bool   `json:"is_admin,omitempty" example:"true"`
 }
-
-type UserData struct {
-	Name         string `json:"name" example:"Иван Иванов"`
-	Email        string `json:"email" example:"ivan@example.com"`
-	PasswordHash string `json:"password_hash" example:"93652657623450"`
-	BirthDate    string `json:"birth_date" example:"1990-01-01"`
-}
-
-type UserLogin struct {
-	Email        string `json:"email" example:"admin@admin.com"`
-	PasswordHash string `json:"password_hash" example:"$2a$10$xS.xH8z3bJ1J5hNtGvXZfez7v6JQY9W7kZf3JvYbW6cXrV1nYd2E3C"`
-}
-
-type UserAdmin struct {
-	IsAdmin bool `json:"is_admin" example:"true"`
-}
-
-type UserRegister struct {
-	Name         string `json:"name" example:"Иван Иванов"`
-	Email        string `json:"email" example:"ivan@example.com"`
-	PasswordHash string `json:"password_hash" example:"hashed_password"`
-	BirthDate    string `json:"birth_date" example:"1990-01-01"`
-}
